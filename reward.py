a8185a03fd972d0789e26d435bc532f307d01384
sourfillet <phillip.sean.sauers@gmail.com>
2025-02-18 18:33:37 -0700

    Updated rewards

--- reward.py
+++ reward.py
@@ -2,9 +2,11 @@ reward_values = {
     # If a level state is loaded, but Link is in the overworld
     'level_state_in_overworld': -2,
     # If Link goes into a new room
-    'new_room': 0,
+    'new_room': 2,
     # If Link moves around the room
-    'movement': 20,
+    'movement': 5,
     # If Link has already been to this state
     'repeat_state': -1,
+    # Reward for killing an enemy
+    'kill_enemy': 1
 }
