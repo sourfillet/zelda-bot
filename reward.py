ec7a8860ef5cc4c9c90f7a9721df22825d7bad7e
sourfillet <phillip.sean.sauers@gmail.com>
2025-02-18 18:33:20 -0700

    Updated DQN

--- reward.py
+++ reward.py
@@ -2,9 +2,9 @@ reward_values = {
     # If a level state is loaded, but Link is in the overworld
     'level_state_in_overworld': -2,
     # If Link goes into a new room
-    'new_room': 2,
+    'new_room': 0,
     # If Link moves around the room
-    'movement': 1.5,
+    'movement': 20,
     # If Link has already been to this state
-    'repeat_state': -.5,
+    'repeat_state': -1,
 }
